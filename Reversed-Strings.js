d86621b15f553ff992f279190549eb71d88ca76f
amvgabo <amvgabo@gmail.com>
2022-07-15 12:08:36 +0800

    added new codewars challenge

---
+++ Reversed-Strings.js
@@ -0,0 +1,12 @@
+//Instruction
+//Complete the solution so that it reverses the string passed into it.
+
+//Examples:
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+//My solution
+
+function solution(str){
+    return str.split("").reverse().join("")
+  }
