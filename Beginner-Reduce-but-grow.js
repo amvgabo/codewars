0c4bca9f633f0d823f71a7793b1de3a31a565e7c
amvgabo <amvgabo@gmail.com>
2022-07-19 00:06:00 +0800

    added new code wars challenge

---
+++ Beginner-Reduce-but-grow.js
@@ -0,0 +1,14 @@
+//Instruction
+
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+
+//My Solution
+
+function grow(x){
+    sum = 1
+    for(let i = 0; i < x.length; i++){
+      sum *= x[i]
+    }
+    return sum
+  }
